d0dc24113965c275fe092a57e8e23e1278d4adb9
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2022-12-01 15:23:24 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -3,53 +3,34 @@ function unlocknav() {
     setTimeout(() => {
         navunlock = true;
     }, 5000);
-}
 
-
-function keypress(clicked_id) {
-
-
-
-    var key = document.querySelector('#' + clicked_id);
-    var color;
-
-    var check = document.querySelector('.keyblack#' + clicked_id);
-    if (check == null) {
-        color = 'var(--pink)';
+    
+    if (navigator.requestMIDIAccess) {
+        console.log('This browser supports WebMIDI!');
+    } else {
+        console.log('WebMIDI is not supported in this browser.');
     }
-    else {
-        color = 'var(--green)';
-    }
-
-    key.style.transitionDuration = '0s';
-    key.style.backgroundColor = color;
-
-    setTimeout(() => {
-        key.style.transitionDuration = '1s';
-        key.style.backgroundColor = 'var(--black)';
-    }, 1); 
-    setpage(clicked_id) 
 }
 
 var navopen = true;
 function usenav() {
     if (navunlock == true) {
         if (navopen == true) {
-            document.querySelector('#navbar').style.transform = 'translatey(100%)'
+            document.querySelector('#navbar').style.transform = 'translatey(10vh)'
             document.querySelector('.keyboard').style.transform = 'translatey(150%)'
             document.querySelector('#helplogo').style.transform = 'translatex(20vw)'
             document.querySelector('#pagecontent').style.top = '15vh'
             document.querySelector('#pagecontent').style.height = '80vh'
-            document.querySelector('#usenavbut').style.transform = 'translatey(100%)'
+            document.querySelector('#usenavbut').style.transform = 'translatey(10vh)'
             navopen = false;
         }
         else {
-            document.querySelector('#navbar').style.transform = 'translatey(0%)'
-            document.querySelector('.keyboard').style.transform = 'translatey(0%)'
-            document.querySelector('#helplogo').style.transform = 'translatex(0%)'
+            document.querySelector('#navbar').style.transform = 'translatey(0)'
+            document.querySelector('.keyboard').style.transform = 'translatey(0)'
+            document.querySelector('#helplogo').style.transform = 'translatex(0)'
             document.querySelector('#pagecontent').style.top = '5vh'
             document.querySelector('#pagecontent').style.height = 'calc(95vh - var(--heightKeyB))'
-            document.querySelector('#usenavbut').style.transform = 'translatey(0%)'
+            document.querySelector('#usenavbut').style.transform = 'translatey(0)'
             
             navopen = true;
         }
@@ -58,8 +39,6 @@ function usenav() {
 
 
 }
-
-
 
 var helpopen = true;
 function usehelp() {
@@ -89,12 +68,39 @@ function usehelp() {
                 }
                 document.querySelector('#pagecontent').style.height = 'calc(95vh - var(--heightKeyB))';
                 document.querySelector('.keyboard').style.bottom = '0vh';
-                document.querySelector('#usenavbut').style.left = '-3vw';
+                document.querySelector('#usenavbut').style.left = '0vw';
                 document.querySelector('#helpoverlay').style.top = '-100vh';      
              }, 500);
             helpopen = true
         }
     }
+}
+
+function keypress(clicked_id) {
+
+    var key = document.querySelector('#' + clicked_id);
+    var color;
+
+    var check = document.querySelector('.keyblack#' + clicked_id);
+    if (check == null) {
+        color = 'var(--pink)';
+    }
+    else {
+        color = 'var(--green)';
+    }
+
+    key.style.transitionDuration = '0s';
+    key.style.backgroundColor = color;
+
+    setTimeout(() => {
+        key.style.transitionDuration = '1s';
+        key.style.backgroundColor = 'var(--black)';
+    }, 1); 
+
+    if (navunlock == true) {
+        setpage(clicked_id) 
+    }
+
 }
 
 document.onkeypress = function(evt) {
