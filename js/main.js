96464c0b022ad1717190f63f2be72530d9717a58
Noud-Janssen <117975100+Noud-Janssen@users.noreply.github.com>
2022-12-01 11:30:15 +0100

    Initial

---
+++ js/main.js
@@ -0,0 +1,96 @@
+var navunlock = false;
+function unlocknav() {
+    setTimeout(() => {
+        navunlock = true;
+    }, 5000);
+}
+
+
+function keypress(clicked_id) {
+    var key = document.querySelector('#' + clicked_id);
+    var color;
+
+
+    var check = document.querySelector('.keyblack#' + clicked_id);
+    if (check == null) {
+        color = 'var(--pink)';
+    }
+    else {
+        color = 'var(--green)';
+    }
+
+    key.style.transitionDuration = '0s';
+    key.style.backgroundColor = color;
+
+    setTimeout(() => {
+        key.style.transitionDuration = '1s';
+        key.style.backgroundColor = 'var(--black)';
+    }, 1); 
+}
+
+var navopen = true;
+function usenav() {
+    if (navunlock == true) {
+        if (navopen == true) {
+            document.querySelector('#navbar').style.transform = 'translatey(100%)'
+            document.querySelector('.keyboard').style.transform = 'translatey(150%)'
+            document.querySelector('#helplogo').style.transform = 'translatex(20vw)'
+            document.querySelector('#butbar').style.backgroundColor = 'var(--pink)'
+            document.querySelector('#butkey').style.background = 'none'
+            document.querySelector('#pagecontent').style.top = '15vh'
+            document.querySelector('#pagecontent').style.height = '80vh'
+            navopen = false;
+        }
+        else {
+            document.querySelector('#navbar').style.transform = 'translatey(0%)'
+            document.querySelector('.keyboard').style.transform = 'translatey(0%)'
+            document.querySelector('#helplogo').style.transform = 'translatex(0%)'
+            document.querySelector('#butkey').style.backgroundColor = 'var(--pink)'
+            document.querySelector('#butbar').style.background = 'none'
+            document.querySelector('#pagecontent').style.top = '5vh'
+            document.querySelector('#pagecontent').style.height = 'calc(95vh - var(--heightKeyB))'
+            navopen = true;
+        }
+    }
+    
+
+
+}
+
+
+
+var helpopen = true;
+function usehelp() {
+    if (navunlock == true) {
+        var nohelp = document.querySelectorAll('.nohelp')
+        if (helpopen == true) {
+            for (i = 0 ; i < nohelp.length ; i++) {
+                nohelp[i].style.opacity = '0';
+            }
+            document.querySelector('#helpoverlay').style.top = 'calc(50vh - var(--heightKeyB))';
+            document.querySelector('#pagecontent').style.height = '0';
+            document.querySelector('.keyboard').style.bottom = 'calc(50vh - var(--heightKeyB) / 2)';
+            document.querySelector('#usenavbut').style.left = '-10vw';
+
+            setTimeout(() => {
+               document.querySelector('#helpoverlay').style.opacity = '1';                
+            }, 1000);
+            helpopen = false
+        }
+        else {
+            
+            
+            document.querySelector('#helpoverlay').style.opacity = '0';
+            setTimeout(() => {
+                for (i = 0 ; i < nohelp.length ; i++) {
+                    nohelp[i].style.opacity = '1';
+                }
+                document.querySelector('#pagecontent').style.height = 'calc(95vh - var(--heightKeyB))';
+                document.querySelector('.keyboard').style.bottom = '0vh';
+                document.querySelector('#usenavbut').style.left = '-3vw';
+                document.querySelector('#helpoverlay').style.top = '-100vh';      
+             }, 500);
+            helpopen = true
+        }
+    }
+}
